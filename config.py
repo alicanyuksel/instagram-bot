2b229f3de8c2768dcbd133f9eec58a52ea196423
Alican Yüksel <alicanyuksel@outlook.com>
2020-12-27 04:33:22 +0300

    private config file added

---
+++ config.py
@@ -0,0 +1,13 @@
+mapbox_api_key = "" # API KEY FOR MAPBOX
+url_api_backend = "" # Our API
+
+# instagram username and password 
+
+ACCOUNT_USERNAME = ""
+ACCOUNT_PASSWORD = ""
+ 
+
+# paths to save the images
+
+PATH_JPG = ""
+PATH_PNG = ""
