e3b4ecf3f581ffd373446423b0a1ad038ada32db
Alican Yüksel <alicanyuksel@outlook.com>
2021-01-02 18:11:50 +0300

    Added some comments for config file

--- config.py
+++ config.py
@@ -2,20 +2,21 @@ mapbox_api_key = "" # API KEY FOR MAPBOX
 url_api_backend = "https://api.depremneredeoldu.com/earthquakes" # Our API
 
 # instagram username and password 
-
 ACCOUNT_USERNAME = ""
 ACCOUNT_PASSWORD = ""
  
 
 # paths to save the images
+# you can put "./" in local but in server, you have to put the entire path
 
 PATH_JPG = ""
 PATH_PNG = ""
 
 # json file
+# idem for the path
 
 earthquakes_detected_path = ""
 earthquakes_posted_path = ""
 
-# configure your hashtag 
+# configure the hashtags for your posts
 hashtag = ""
