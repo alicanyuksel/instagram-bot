e57bc206acdf819659272337c71dc205016f354f
Alican Yüksel <alicanyuksel@outlook.com>
2021-01-03 18:30:32 +0300

    Changed the term used in Turkish for the term 'magnitude'

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ if new_earthquakes_to_post:
 
     count = 0
     for earthquake_id, earthquake_info in reversed(list(new_earthquakes_to_post.items())):
-        description_earthquake = f"""\nLokasyon : {earthquake_info['location']}\nŞiddet : {earthquake_info['magnitude']}\nDerinlik : {earthquake_info['depth']}\nTarih - Saat : {earthquake_info['date']} - {earthquake_info['time']}\n\nDaha fazla detay için : depremneredeoldu.com\n\n{hashtag}"""
+        description_earthquake = f"""\nLokasyon : {earthquake_info['location']}\nBüyüklük : {earthquake_info['magnitude']}\nDerinlik : {earthquake_info['depth']}\nTarih - Saat : {earthquake_info['date']} - {earthquake_info['time']}\n\nDaha fazla detay için : depremneredeoldu.com\n\n{hashtag}"""
 
         PATH = f'{PATH_JPG}/{earthquake_id}.jpg'
 
