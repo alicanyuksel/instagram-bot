705c261eae4131beaa9e152aaf773184cc4b9bad
Alican Yüksel <alicanyuksel@outlook.com>
2021-01-17 13:20:17 +0300

    Some adaptations PEP-8

--- bot.py
+++ bot.py
@@ -6,13 +6,12 @@ import logging
 # intern package
 from config import (
     ACCOUNT_USERNAME,
-    ACCOUNT_PASSWORD, 
-    PATH_JPG, 
-    JSON_PATH, 
+    ACCOUNT_PASSWORD,
+    PATH_JPG,
+    JSON_PATH,
     hashtag
 )
 from utils import *
-
 
 # detect the earthquakes and save them into json file
 earthquakes_detected = detect_earthquakes(data)
@@ -20,7 +19,7 @@ earthquakes_detected = detect_earthquakes(data)
 # save maps generated from MapBox API
 save_maps_images(earthquakes_detected)
 
-# create the log files
+#  create the log files
 log = open("log.txt", "a+")
 logging.basicConfig(filename='log_bot.log')
 
@@ -36,7 +35,6 @@ if not os.path.exists(JSON_PATH["earthquakes_posted"]):
 json_file_posted = open(JSON_PATH["earthquakes_posted"])
 earthquakes_posted = json.load(json_file_posted)
 
-
 # Get all of the ids of earthquakes posted if earthquakes_posted is not empty file
 posted_earthquakes_ids = list(earthquakes_posted.keys() if earthquakes_posted else "")
 
@@ -46,15 +44,14 @@ for earthquake_id, earthquake_info in earthquakes_detected.items():
     if earthquake_id not in posted_earthquakes_ids:
         new_earthquakes_to_post[earthquake_id] = earthquake_info
 
-
 if new_earthquakes_to_post:
-    #login
-    cl = Client()       
+    # login
+    cl = Client()
     cl.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD)
 
     # logging 
     cl.request_logger = logging.getLogger("private_request")
-    
+
     count = 0
     for earthquake_id, earthquake_info in reversed(list(new_earthquakes_to_post.items())):
         description_earthquake = f"""\nLokasyon : {earthquake_info['location']}\nBüyüklük : {earthquake_info['magnitude']}\nDerinlik : {earthquake_info['depth']}\nTarih - Saat : {earthquake_info['date']} - {earthquake_info['time']}\n\nDaha fazla detay için : depremneredeoldu.com\n\n{hashtag}"""
@@ -64,15 +61,15 @@ if new_earthquakes_to_post:
         # set timeout 
         cl.request_timeout = 5
 
-        cl.photo_upload(PATH,                 
+        cl.photo_upload(PATH,
                         caption=description_earthquake)
 
         with open(JSON_PATH["earthquakes_posted"], "r+") as file:
             data = json.load(file)
-            data.update({earthquake_id : earthquake_info})
+            data.update({earthquake_id: earthquake_info})
             file.seek(0)
             json.dump(data, file, indent=4)
 
         count += 1
-        
+
     log.write(f"{count} file posted ! - {datetime.datetime.now()}\n")
--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ import datetime
 # intern package
 from config import mapbox_api_key, url_api_backend, PATH_PNG, PATH_JPG, JSON_PATH
 
-# to create an id unique, we will use the date, time and magnitude concataneted without punctuation
+# to create an id unique, we will use the date, time and magnitude concatenated without punctuation
 exclude = string.punctuation
 
 # get the data from Deprem Nerede Oldu API
@@ -20,9 +20,8 @@ log = open("log.txt", "a+")
 
 
 def create_json(dictionary, path):
-    with open(path, "w") as fl:  
-        json_object = json.dump(dictionary, fl, indent = 4)
-
+    with open(path, "w") as fl:
+        json_object = json.dump(dictionary, fl, indent=4)
 
 
 def safe_open(path, mode):
@@ -34,7 +33,6 @@ def safe_open(path, mode):
     return open(path, mode)
 
 
-
 def detect_earthquakes(data):
     earthquakes_detected = {}
     for earthquake in data["earthquakes"]:
@@ -43,22 +41,19 @@ def detect_earthquakes(data):
             date_time_magnitude = f"{earthquake['date']}-{earthquake['time']}-{earthquake['magnitude']}"
 
             earthquake_id = "".join([ch for ch in date_time_magnitude if ch not in exclude])
-            
-            earthquakes_detected[earthquake_id] = earthquake
 
+            earthquakes_detected[earthquake_id] = earthquake
 
     create_json(dictionary=earthquakes_detected, path=JSON_PATH["earthquakes_detected"])
 
     return earthquakes_detected
-    
-    
+
 
 def genereate_earthquakes_images(earthquake_id, earthquake_detail, mapbox_api_key):
     all_png_files = [os.path.basename(image_file).split(".png")[0] for image_file in glob.glob(f"{PATH_PNG}/*.png")]
 
     if earthquake_id not in all_png_files:
-        img = safe_open(f"{PATH_PNG}/{earthquake_id}.png","wb")
-        
+        img = safe_open(f"{PATH_PNG}/{earthquake_id}.png", "wb")
 
         ACCESS_TOKEN = mapbox_api_key
         latitude = earthquake_detail["latitude"]
@@ -78,10 +73,9 @@ def genereate_earthquakes_images(earthquake_id, earthquake_detail, mapbox_api_ke
         cv2.imwrite(f'{PATH_JPG}/{earthquake_id}.jpg', image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
 
         return True
-    
+
     else:
         return False
-
 
 
 def save_maps_images(earthquakes_detected):
@@ -93,14 +87,14 @@ def save_maps_images(earthquakes_detected):
 
     count = 0
     for key, earthquake_info in earthquakes_detected.items():
-            result = genereate_earthquakes_images(
-                earthquake_id=key,
-                earthquake_detail=earthquake_info,
-                mapbox_api_key=mapbox_api_key
-            )
+        result = genereate_earthquakes_images(
+            earthquake_id=key,
+            earthquake_detail=earthquake_info,
+            mapbox_api_key=mapbox_api_key
+        )
 
-            if result:
-                count+=1
+        if result:
+            count += 1
 
     if count > 0:
         log.write(f"{count} new images created ! - {datetime.datetime.now()}\n")
